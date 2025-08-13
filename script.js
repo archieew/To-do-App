9203ca6f1b8e3a205bf9949c7d2dd3e33942c5d1
Archie Custodio <archie06custodio@gmail.com>
2025-08-13 17:40:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,6 @@ const dateInput = document.getElementById("date-input");
 const descriptionInput = document.getElementById("description-input");
 
 const taskData = [];
-
 let currentTask = {};
+
+openTaskFormBtn.addEventListener("click",() => taskForm.classList.toggle("hidden"))
