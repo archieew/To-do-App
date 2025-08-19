75003a5fe822261700b965175c8dde5e1bb6c5d5
Archie Custodio <archie06custodio@gmail.com>
2025-08-19 19:48:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,9 @@ taskForm.addEventListener("submit", (e) => {
     description: descriptionInput.value,
   };
 
-  if(dataArrIndex === -1){
-    taskData.unshift(taskObj)
+  if (dataArrIndex === -1) {
+    taskData.unshift(taskObj);
   }
+
+  taskData.forEach(({id, title, date, description}) =>{})
 });
