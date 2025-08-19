995be97f566ed8f3ed7ebde3e3fd9ea8fff41f02
Archie Custodio <archie06custodio@gmail.com>
2025-08-19 22:01:45 +0800

    Update script.js

--- script.js
+++ script.js
@@ -45,13 +45,15 @@ taskForm.addEventListener("submit", (e) => {
   }
 
   taskData.forEach(({id, title, date, description}) => {
-        tasksContainer.innerHTML += `
-          <div class="task" id="${id}">
-            <p><strong>Title:</strong> ${title}</p>
-            <p><strong>Date:</strong> ${date}</p>
-            <p><strong>Description:</strong> ${description}</p>
-          </div>
-        `
-      }
-    );
+      tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+        <button type="button" class="btn">Edit</button> 
+        <button type="button" class="btn">Delete</button>   
+        </div>
+      `
+    }
+  );
 });
