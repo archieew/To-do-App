5a970f5105c2383854cdfea8a5555f5bb5c0db07
Archie Custodio <archie06custodio@gmail.com>
2025-08-19 22:04:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -50,10 +50,12 @@ taskForm.addEventListener("submit", (e) => {
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
           <p><strong>Description:</strong> ${description}</p>
-        <button type="button" class="btn">Edit</button> 
-        <button type="button" class="btn">Delete</button>   
+          <button type="button" class="btn">Edit</button>
+          <button type="button" class="btn">Delete</button>
         </div>
       `
     }
   );
+taskForm.classList.toggle("hidden")
 });
+
